e7ba44f2b2f38e821812686bf995f1cd2a3161b5
sabah1994 <sabah1994786@gmail.com>
2021-12-09 23:00:50 +0000

    changes to dev_tmux

--- utils/dev_tmux.sh
+++ utils/dev_tmux.sh
@@ -2,9 +2,9 @@
 #Default args
 path=${1:-"/home/sabah/Desktop/development"}
 export_creds='export GOOGLE_APPLICATION_CREDENTIALS="/Users/uddins/Desktop/dev-pipeline.json"'
-# Session Name
-# session="some_session"
 session=${2:-"some_session"}
+
+activate_venv="source venv/bin/activate"
 
 #check if session exists
 SESSIONEXISTS=$(tmux list-sessions | grep $session)
@@ -12,36 +12,32 @@ if [ "$SESSIONEXISTS" != "" ]
 then
     read -r -p "Session alaready :$session: exists, drop and recreate? [y/n]" response
     # if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
-    if [ $response="y" ]
+    if [ $response = "y" ]
     then echo killed session :$session; $(tmux kill-session -t $session)
     else echo Did not kill session; exit 1
     fi
 fi
+
+# Create new session
 tmux new-session -d -s $session
 echo Created session :$session
 
-# First window
+# First window, install necessary plugins needed for vim to function
 tmux rename-window -t 0 'Main'
-tmux send-keys -t  $session:Main.0 "tmux split-window -v" ENTER
-sleep 3
-tmux send-keys -t  $session:Main.1 "tmux resize-pan -D 15" ENTER
-#cd to directory and open nvim
-tmux send-keys -t  $session:Main.0 "cd $path && nvim" ENTER
+tmux send-keys -t  $session:Main.0 "cd $path && $activate_venv && $export_creds && pip install jedi black flake8 neovim; tmux split-window -v; nvim" ENTER
 tmux send-keys -t  $session:Main.0 ":NT" ENTER
-tmux send-keys -t  $session:Main.1 "cd $path" ENTER
-#if there is virtualenv, activate it
-tmux send-keys -t  $session:Main.1 "source venv/bin/activate && $export_creds" ENTER
+sleep 3
+tmux send-keys -t  $session:Main.1 "cd $path && $activate_venv; tmux resize-pan -D 15" ENTER
 echo configured first window
 
 
 #Second window
 tmux new-window -t $session:1 -n 'Secondary'
-tmux send-keys -t  $session:Secondary.0 "tmux split-window -v" ENTER
+tmux send-keys -t  $session:Secondary.0 "tmux split-window -v; nvim" ENTER
+tmux send-keys -t  $session:Secondary.0 ":NT" ENTER
 sleep 3
 tmux send-keys -t  $session:Secondary.1 "tmux resize-pan -D 15" ENTER
-tmux send-keys -t  $session:Secondary.0 "nvim" ENTER
-tmux send-keys -t  $session:Secondary.0 ":NT" ENTER
-# echo configured second window
 # sleep 2
+
 #attach to session
 tmux attach-session -t $SESSION:0.0
